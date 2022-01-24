053c28f62eb91ba74604351480e601521e8ad53e
giannibalistreri <gbalistreri762@gmail.com>
2022-01-24 22:04:46 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,48 @@
+import setuptools
+import subprocess
+import sys
+
+# Install jupyter notebook extensions:
+subprocess.run(['python{} -m pip install jupyter_contrib_nbextensions && jupyter contrib nbextension install'.format('3' if sys.platform.find('win') != 0 else '')], shell=True)
+
+with open('README.md', 'r') as _read_me:
+    long_description = _read_me.read()
+
+with open('requirements.txt', 'r') as _requirements:
+    requires = _requirements.read()
+
+requires = [r.strip() for r in requires.split('\n') if ((r.strip()[0] != "#") and (len(r.strip()) > 3) and "-e git://" not in r)]
+
+setuptools.setup(
+    name='document_denoising',
+    version='0.0.1',
+    author='Gianni Francesco Balistreri',
+    author_email='gbalistreri@gmx.de',
+    description='Machine learning algorithms for denoising document images',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='computer vision machine learning gan autoencoder deep learning',
+    license='GNU',
+    url='https://github.com/GianniBalistreri/document_denoising',
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    package_data={'document_denoising': ['LICENSE',
+                                         'README.md',
+                                         'requirements.txt',
+                                         'setup.py'
+                                         ]
+                  },
+    data_file=[('test', [
+                         ]
+                )],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=requires
+)
